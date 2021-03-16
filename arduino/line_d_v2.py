ecfbc9bb30d177617e233047eb3d864a5981428c
PhoenixIIexe <sashaerg10.07.6@mail.ru>
2021-03-16 19:09:08 +0000

    Полностью новое line_d!!!:)

--- arduino/line_d_v2.py
+++ arduino/line_d_v2.py
@@ -7,56 +7,61 @@ from time import sleep, time
 hsv_min = np.array((0, 0, 100), np.uint8)
 hsv_max = np.array((255, 255, 255), np.uint8)
 
-distortions = [1000, 0, 0]
-normal = [1000, 0, 0]
-k = 0
-def main():
+sensor_d = {
+    'line': 0,
+    'cr': 0
+}
+
+def sensor():
     #img = cv.imread(fn)[0+50: cv.imread(fn).shape[1]-50, 0+50: cv.imread(fn).shape[0]-70]
-    img = cv.imread('line_3.png')
+    img = cv.imread('ard_2.png')
     # img = cv.VideoCapture(0).read()[1]
+    sensor_d['cr'] = 101
+    normal_y, distortions_y, distortions_r, distortions_l = [1000, 0, 0, 0]
 
     hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
     thresh = cv.inRange(hsv, hsv_min, hsv_max)
 
     contours, hierarchy = cv.findContours( thresh.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-    for i in contours[1][5:-5]:
-        x = i[0][0]
-        if abs(254 - x) < 65: # 254 130
-            if normal[0] > x:
-                normal[0] = x
-            elif normal[1] < x:
-                normal[1] = x
-                normal[2] = i[0][1]
-        else:
-            if distortions[0] > x:
-                distortions[0] = x
-            elif distortions[1] < x:
-                distortions[1] = x
-                distortions[2] = i[0][1]
+    default = contours[1][-3][0][0] - contours[1][-4][0][0]
+    for i in range(4, len(contours[1])//2):
 
-    print(normal, '\n\n', distortions)
-    if distortions == [1000, 0, 0]:
-        print('Линия')
-    elif abs(normal[1] - distortions[1]) > 20 and abs(normal[0] - distortions[0]) < 20:
-        if abs(distortions[2] - normal[2]) > 20:
-            print('Изгиб вправо')
-        else:
-            print('Г-образный изгиб вправо')
-    elif abs(normal[1] - distortions[1]) < 20 and abs(normal[0] - distortions[0]) > 20:
-        if abs(distortions[2] - normal[2]) > 20:
-            print('Изгиб влево')
-        else:
-            print('Г-образный изгиб влево')
-    elif abs(normal[1] - distortions[1]) > 20 and abs(normal[0] - distortions[0]) > 20:
-        print('Перекресток')
+        if -20 <= contours[1][i][0][0] - contours[1][-(i+1)][0][0] - default <= 20:
+            if contours[1][i][0][1] < normal_y:
+                normal_y = contours[1][i][0][1]
+            default = abs(contours[1][-(i+1)][0][0] - contours[1][i][0][0])
+            if contours[1][i][0][0] < 200:
+                sensor_d['line'] = -1
+            elif contours[1][-(i+1)][0][0] > 200:
+                sensor_d['line'] = 1
+            else:
+                if contours[1][i][0][0] - 200 < 5:
+                    sensor_d['line'] = -1
+                elif 200 - contours[1][-(i+1)][0][0] < 5:
+                    sensor_d['line'] = 1
+                else:
+                    sensor_d['line'] = 0
 
-    cv.drawContours( img, contours, -1, (0, 255, 255), 3, cv.LINE_AA, hierarchy, 2)
-    cv.imshow('contours', img)
+        elif contours[1][i][0][0] - contours[1][-(i+1)][0][0]  - default > 20 and distortions_r + distortions_l < 350:
+            if abs(contours[1][-(i+1)][0][0] - contours[1][i][0][0]) > distortions_r:
+                distortions_r = abs(contours[1][-(i+1)][0][0] - contours[1][i][0][0])
+            distortions_y = contours[1][i][0][1]
+            sensor_d['cr'] = 1
 
-    cv.waitKey()
-    cv.destroyAllWindows()
+        elif contours[1][i][0][0] - contours[1][-(i+1)][0][0] - default < -20 and distortions_r + distortions_l < 350:
+            if abs(contours[1][-(i+1)][0][0] - contours[1][i][0][0]) > distortions_l:
+                distortions_l = abs(contours[1][-(i+1)][0][0] - contours[1][i][0][0])
+            distortions_y = contours[1][i][0][1]
+            sensor_d['cr'] = -1
 
-temp = round(time())
-while round(time()) - temp < 1:
-    main()
+        elif distortions_r + distortions_l >= 350:
+            sensor_d['cr'] = 0
+            break
+
+
+    if sensor_d['cr'] in [-1, 1] and abs(distortions_y - normal_y) > 20:
+        sensor_d['cr'] = -2 if sensor_d['cr'] == -1 else 2
+
+    print(sensor_d)
+    return sensor_d
