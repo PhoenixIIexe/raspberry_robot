9e7b2d59df927b4504eaaed2ba001a4c9b3a6ef7
PhoenixIIexe <sashaerg10.07.6@mail.ru>
2021-03-22 19:56:22 +0000

    Полностью переделанный line_d без циклов

--- arduino/line_d.py
+++ arduino/line_d.py
@@ -1,7 +1,6 @@
 import sys
 import numpy as np
 import cv2 as cv
-from time import sleep, time
 
 
 hsv_min = np.array((0, 0, 100), np.uint8)
@@ -12,75 +11,40 @@ sensor_d = {
     'cr': 0
 }
 
-
-img = cv.warpAffine(cv.VideoCapture(0).read()[1], cv.getRotationMatrix2D((320, 240), 180, 1.0), (640, 480))[360: 480, 120: 520]
-hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-thresh = cv.inRange(hsv, hsv_min, hsv_max)
-
-contours, hierarchy = cv.findContours( thresh.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-m_line = contours[1][7][0][0]
-print(m_line)
-
-
 def sensor():
-    distortions = []
-    normal = []
-    normal_y, distortions_y = 0, 0
-    sensor_d['cr'] = 101
-
-
     img = cv.warpAffine(cv.VideoCapture(0).read()[1], cv.getRotationMatrix2D((320, 240), 180, 1.0), (640, 480))
     img = img[360: 480, 120: 520]
 
+    sensor_d['cr'] = 101
+    normal_y, distortions_y, distortions_r, distortions_l = [1000, 0, 0, 0]
+
     hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
     thresh = cv.inRange(hsv, hsv_min, hsv_max)
-    
+
     contours, hierarchy = cv.findContours( thresh.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-    try:
-      for i in contours[1]:
-          x = i[0][0]
-          if abs(m_line - x) < 100 :
-              if distortions != [] and normal.count(x) == 1:
-                  distortions.append(x)
-                  distortions_y = i[0][1]
-              else:
-                  normal.append(x)
-                  normal_y = i[0][1]
-          else:
-              distortions.append(x)
-              distortions_y = i[0][1]
-      print(normal, distortions)
-      try:         
-          if abs(max(normal) - max(distortions)) > 20 and abs(min(normal) - min(distortions)) < 20:
-              if abs(distortions_y - normal_y) > 20:
-                  sensor_d['cr'] = 2
-              else:
-                  sensor_d['cr'] = 1
-          elif abs(max(normal) - max(distortions)) < 20 and abs(min(normal) - min(distortions)) > 20:
-              if abs(distortions_y - normal_y) > 20:
-                  sensor_d['cr'] = -2
-              else:
-                  sensor_d['cr'] = -1
-          elif abs(max(normal) - max(distortions)) > 20 and abs(min(normal) - min(distortions)) > 20:
-              sensor_d['cr'] = 0
-      except:
-          pass
-      finally:
-          sensor = normal if normal != [] else distortions
-          if max(sensor) < m_line:
-              sensor_d['line'] = -1
-          elif min(sensor) > m_line:
-              sensor_d['line'] = 1
-          else:
-              if (max(sensor)+min(sensor))/2 - m_line < -20:
-                  sensor_d['line'] = -1
-              elif (max(sensor)+min(sensor))/2 - m_line > 20:
-                  sensor_d['line'] = 1
-              else:
-                  sensor_d['line'] = 0
-    except:
-      pass
+    if contours != []:
+        contours = ' '.join(''.join(''.join(''.join(list(map(str, contours[1][4:-3]))).split('[['))).split(']]')).split(' ')
+        contours = list(map(int, list(filter(None, contours))[::2]))
+        
+        if sum(contours)//len(contours) < 200:
+            sensor_d['line'] = -1
+        elif sum(contours)//len(contours) > 200:
+            sensor_d['line'] = 1
+        else:
+            if sum(contours)//len(contours) - 200 < -20:
+                sensor_d['line'] = -1
+            elif sum(contours)//len(contours) - 200 > 20:
+                sensor_d['line'] = 1
+            else:
+                sensor_d['line'] = 0
 
+        if abs(min(contours) - sum(contours)//len(contours)) > 20 and abs(max(contours) - sum(contours)//len(contours)) < 100:
+            sensor_d['cr'] = -1
+        elif abs(min(contours) - sum(contours)//len(contours)) < 100 and abs(max(contours) - sum(contours)//len(contours)) > 20:
+            sensor_d['cr'] = 1
+        elif abs(min(contours) - sum(contours)//len(contours)) > 100 and abs(max(contours) - sum(contours)//len(contours)) > 100:
+            sensor_d['cr'] = 0
+        
     print(sensor_d)
     return sensor_d
